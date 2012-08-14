b515f680472137da5a58d2776daf63122c8e26ef
Coen De Roover <cderoove@vub.ac.be>
2012-08-15 01:42:48 +0200

    brought the example repl session a bit more up to date

--- EkekoPlugin/src/damp/ekeko.clj
+++ EkekoPlugin/src/damp/ekeko.clj
@@ -107,8 +107,7 @@
   
   (ekeko* [?node ?child ?keyword] (child ?keyword ?node ?child)) 
   
-  (ekeko* [?cu ?exp] (ast :MethodDeclaration ?cu) (child+ ?cu ?exp) (succeeds (actual-expression? ?exp)))   
-  
+  (ekeko* [?cu ?exp] (ast :MethodDeclaration ?cu) (child+ ?cu ?exp) (succeeds (damp.ekeko.jdt.astnode/actual-expression? ?exp)))   
   
   (ekeko* [?inv] (ast :MethodInvocation ?inv) (has :expression ?inv nil))
   
@@ -135,128 +134,128 @@
   (ekeko* [?import ?package ?components] (import-declaration-imports-package ?import ?package) (equals ?components (vec (.getNameComponents ?package))))
     
   
-(ekeko* [?qualifiedPackageName ?qualifiedTypeName ?typeNode] 
-        (fresh [?typeBinding ?packageBinding]
-               (ast :Type ?typeNode) 
-               (!= nil ?typeBinding)
-               (equals ?typeBinding (.resolveBinding ?typeNode))
-               (equals false (.isFromSource ?typeBinding))
-               (equals false (.isPrimitive ?typeBinding))
-               (equals ?qualifiedTypeName (.getQualifiedName ?typeBinding))
-               (!= nil ?packageBinding)
-               (equals ?packageBinding (.getPackage ?typeBinding))
-               (equals ?qualifiedPackageName (.getName ?packageBinding)) 
-               ))
+  (ekeko* [?qualifiedPackageName ?qualifiedTypeName ?typeNode] 
+          (fresh [?typeBinding ?packageBinding]
+                 (ast :Type ?typeNode) 
+                 (!= nil ?typeBinding)
+                 (equals ?typeBinding (.resolveBinding ?typeNode))
+                 (equals false (.isFromSource ?typeBinding))
+                 (equals false (.isPrimitive ?typeBinding))
+                 (equals ?qualifiedTypeName (.getQualifiedName ?typeBinding))
+                 (!= nil ?packageBinding)
+                 (equals ?packageBinding (.getPackage ?typeBinding))
+                 (equals ?qualifiedPackageName (.getName ?packageBinding)) 
+                 ))
   
   
-   (ekeko* [?m ?cfg] (method-cfg ?m ?cfg))
-   
-   (ekeko* [?m ?cfg ?entry ?end]
-           (method-cfg ?m ?cfg) 
-           (method-cfg-entry ?m ?entry)
-           (fresh [?beforeReturn ?return]
-                  (project [?cfg]
-                           (qwal ?cfg ?entry ?end 
-                                 []
-                                 (qcurrent [currentStatement] (equals currentStatement ?beforeReturn))
-                                 q=>
-                                 (qcurrent [currentStatement] (equals currentStatement ?return) (ast :ReturnStatement ?return))))))
-   
-    (ekeko-n* 100 [?m ?beforeReturn ?return]
-                 (fresh [?end ?entry ?cfg]
-                        (method-cfg ?m ?cfg)
-                        (method-cfg-entry ?m ?entry)
-                        (project [?cfg]
-                                 (qwal ?cfg ?entry ?end 
-                                       []
-                                       (q=>*)
-                                       (qcurrent [currentStatement] (equals currentStatement ?beforeReturn))
-                                       (q=>*)
-                                       (qcurrent [currentStatement] (equals currentStatement ?beforeReturn))
-                                       (q=>*)
-                                       (qcurrent [currentStatement] (equals currentStatement ?return) (ast :ReturnStatement ?return))))))
-   
-    ;following assume whole-program analyses have been enabled on a single project
-    (ekeko* [?c] (soot :class ?c))
-    
-    (ekeko* [?m] (soot :method ?m))
-    
-    (ekeko* [?c ?m] (soot-class-method ?c ?m))
-    
-    (ekeko* [?c ?f] (soot-class-field ?c ?f))
-    
-    (ekeko* [?m ?b] (soot-method-body ?m ?b))
-
-    (ekeko* [?m ?cfg] (soot-method-cfg ?m ?cfg))
-    
-    (ekeko* [?m ?cfg ?entry] (soot-method-cfg-entry ?m ?cfg ?entry))
-    
-    (ekeko* [?m ?cfg ?exit] (soot-method-cfg-exit ?m ?cfg ?exit))
-    
-    
-    ;Query quantifies over all paths through a SOOT CFG from an entry point ?entry to an exit point ?exit where the exit point uses a value ?defval defined by a previous Soot statement ?unit that uses a value ?usedval of type ?keyw.
-    (ekeko*  [?m ?entry ?exit ?unit ?keyw]
-             (fresh [?cfg ?defbox ?exitbox ?usebox ?defval ?usedval]
-                    (soot-method-cfg-entry ?m ?cfg ?entry)
-                    (soot-method-cfg-exit ?m ?cfg ?exit)
-                    (project [?cfg]
+  (ekeko* [?m ?cfg] (method-cfg ?m ?cfg))
+  
+  (ekeko* [?m ?cfg ?entry ?end]
+          (method-cfg ?m ?cfg) 
+          (method-cfg-entry ?m ?entry)
+          (fresh [?beforeReturn ?return]
+                 (qwal ?cfg ?entry ?end []
+                       (qcurrent [currentStatement]
+                                 (equals currentStatement ?beforeReturn))
+                       q=>
+                       (qcurrent [currentStatement]
+                                 (equals currentStatement ?return)
+                                 (ast :ReturnStatement ?return)))))
+  
+  (ekeko-n* 100 [?m ?beforeReturn ?return]
+            (fresh [?end ?entry ?cfg]
+                   (method-cfg ?m ?cfg)
+                   (method-cfg-entry ?m ?entry)
+                   (qwal ?cfg ?entry ?end 
+                         []
+                         (q=>*)
+                         (qcurrent [currentStatement] (equals currentStatement ?beforeReturn))
+                         (q=>*)
+                         (qcurrent [currentStatement] (equals currentStatement ?beforeReturn))
+                         (q=>*)
+                         (qcurrent [currentStatement] (equals currentStatement ?return) (ast :ReturnStatement ?return)))))
+  
+  ;following assume whole-program analyses have been enabled on a single project
+  (ekeko* [?c] (soot :class ?c))
+  
+  (ekeko* [?m] (soot :method ?m))
+  
+  (ekeko* [?c ?m] (soot-class-method ?c ?m))
+  
+  (ekeko* [?c ?f] (soot-class-field ?c ?f))
+  
+  (ekeko* [?m ?b] (soot-method-body ?m ?b))
+  
+  (ekeko* [?m ?cfg] (soot-method-cfg ?m ?cfg))
+  
+  (ekeko* [?m ?cfg ?entry] (soot-method-cfg-entry ?m ?cfg ?entry))
+  
+  (ekeko* [?m ?cfg ?exit] (soot-method-cfg-exit ?m ?cfg ?exit))
+  
+  
+  ;Query quantifies over all paths through a SOOT CFG from an entry point ?entry to an exit point ?exit where the exit point uses a value ?defval defined by a previous Soot statement ?unit that uses a value ?usedval of type ?keyw.
+  (ekeko*  [?m ?entry ?exit ?unit ?keyw]
+           (fresh [?cfg ?defbox ?exitbox ?usebox ?defval ?usedval]
+                  (soot-method-cfg-entry ?m ?cfg ?entry)
+                  (soot-method-cfg-exit ?m ?cfg ?exit)
                              (qwal ?cfg ?entry ?exit 
                                    []
                                    (q=>*)
                                    (qcurrent [curr] 
-                                     (equals curr ?unit) 
-                                     (soot-unit-defbox ?unit ?defbox) 
-                                     (soot-valuebox-value ?defbox ?defval) 
-                                     (soot-unit-usebox ?unit ?usebox)
-                                     (soot-valuebox-value ?usebox ?usedval) 
-                                     (soot-value ?keyw ?usedval))
+                                             (equals curr ?unit) 
+                                             (soot-unit-defbox ?unit ?defbox) 
+                                             (soot-valuebox-value ?defbox ?defval) 
+                                             (soot-unit-usebox ?unit ?usebox)
+                                             (soot-valuebox-value ?usebox ?usedval) 
+                                             (soot-value ?keyw ?usedval))
                                    (q=>+)
                                    (qcurrent [curr]
-                                     (equals curr ?exit) 
-                                     (soot-unit-usebox ?exit ?exitbox) 
-                                     (soot-valuebox-value ?exitbox ?defval))))))
-
-    (ekeko*  [?ast ?unit ?local] 
-             (fresh [?model]
-                    (ast-reference-model-soot-unit-local ?ast ?model ?unit ?local)))
-             
-            
-    ;Eclipse<->Soot mapping is still very basic
-    (ekeko*  [?ast ?unit ?local ?unit1 ?local1] 
-             (fresh [?model]
-                    (ast-reference-model-soot-unit-local ?ast ?model ?unit ?local)
-                    (ast-reference-model-soot-unit-local ?ast ?model ?unit1 ?local1)
-                    (!= ?local ?local1)))
-    
-    (ekeko*  [?ast ?set]
-              (fresh [?model]
-                     (ast-reference-soot-model-points-to ?ast ?model ?set)))
-    
-    ;DANGER: slow on non-trivial programs
-    (ekeko-n* 100
-              [?ast1 ?ast2]
-              (fresh [?model]
-                     (ast-references-soot-model-may-alias ?ast1 ?ast2 ?model)))
-    
-    
-    
-    ;TODO: following gives no results
-   ;     (ekeko* [?m ?p ?owner ?parentKeyInParent ?grandParentNode ?parentNode]
-   ;        (ast :MethodDeclaration ?m) (child :parameters ?m ?p) (has ?parentKeyInParent ?parentNode ?p))
-    ;while the child-variants loops
-    ;        (ekeko* [?m ?p ?owner ?parentKeyInParent ?grandParentNode ?parentNode]
-    ;       (ast :MethodDeclaration ?m) (child :parameters ?m ?p) (child ?parentKeyInParent ?parentNode ?p))
-
-    
-    ;manually determine ProjectModel instances to be queried 
-    ;(will be filtered to JavaProjectModel instances by ast reification predicates)
-    (binding [*queried-project-models* 
-              (filter (fn [project-model] 
-                        (= "JHotDraw51" (.getName (.getProject project-model))))
-                      (all-project-models))]
-      (ekeko* [?cu] (ast :CompilationUnit ?cu)))
-                        
-                        
-  )
+                                             (equals curr ?exit) 
+                                             (soot-unit-usebox ?exit ?exitbox) 
+                                             (soot-valuebox-value ?exitbox ?defval)))))
+  
+  (ekeko*  [?ast ?unit ?local] 
+           (fresh [?model]
+                  (ast-reference-model-soot-unit-local ?ast ?model ?unit ?local)))
+  
+  
+  ;Eclipse<->Soot mapping is still very basic
+  (ekeko*  [?ast ?unit ?local ?unit1 ?local1] 
+           (fresh [?model]
+                  (ast-reference-model-soot-unit-local ?ast ?model ?unit ?local)
+                  (ast-reference-model-soot-unit-local ?ast ?model ?unit1 ?local1)
+                  (!= ?local ?local1)))
+  
+  (ekeko*  [?ast ?set]
+           (fresh [?model]
+                  (ast-reference-soot-model-points-to ?ast ?model ?set)))
+  
+  ;DANGER: slow on non-trivial programs
+  (ekeko-n* 100
+            [?ast1 ?ast2]
+            (fresh [?model]
+                   (ast-references-soot-model-may-alias ?ast1 ?ast2 ?model)))
+  
+  
+  
+  ;TODO: following gives no results
+  ;     (ekeko* [?m ?p ?owner ?parentKeyInParent ?grandParentNode ?parentNode]
+  ;        (ast :MethodDeclaration ?m) (child :parameters ?m ?p) (has ?parentKeyInParent ?parentNode ?p))
+  ;while the child-variants loops
+  ;        (ekeko* [?m ?p ?owner ?parentKeyInParent ?grandParentNode ?parentNode]
+  ;       (ast :MethodDeclaration ?m) (child :parameters ?m ?p) (child ?parentKeyInParent ?parentNode ?p))
+  
+  
+  ;manually determine ProjectModel instances to be queried 
+  ;(will be filtered to JavaProjectModel instances by ast reification predicates)
+  (binding [damp.ekeko.ekekomodel/*queried-project-models* 
+            (filter (fn [project-model] 
+                      (= "JHotDraw51" (.getName (.getProject project-model))))
+                    (all-project-models))]
+    (ekeko* [?cu] (ast :CompilationUnit ?cu)))
+  
+  
+  
+  )    
 
 

3d0b926c1ad29812b1f947e1d5c5f1eecd1d7804
Coen De Roover <cderoove@vub.ac.be>
2012-09-07 11:31:05 -0300

    Moved AspectJ folder to its own repo.

--- EkekoAspectJ/src/damp/ekeko/aspectj/projectmodel.clj
+++
@@ -1,8 +0,0 @@
-(ns damp.ekeko.aspectj.projectmodel
-  (:import 
-    [damp.ekeko.aspectj AspectJProjectModel])
-  (:require
-    [damp.ekeko [ekekomodel :as ekekomodel]]))
-
-
-;(ekekomodel/queried-project-models)

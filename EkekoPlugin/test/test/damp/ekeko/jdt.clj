6366b521b6ab5c010f63e3655a62e24a2058dc66
Coen De Roover <cderoove@vub.ac.be>
2012-11-14 20:10:36 +0100

    Expanding tests for JDT-related relations.

--- EkekoPlugin/test/test/damp/ekeko/jdt.clj
+++ EkekoPlugin/test/test/damp/ekeko/jdt.clj
@@ -12,26 +12,45 @@
 ;; Tests
 
 (deftest
-  reification-test
-  (is 
-    (reduce
-      (fn [sofar
-           [cu key child shouldbecu shouldbekey]]
-        (and sofar 
-             (identical? cu shouldbecu)
-             (identical? key shouldbekey)))
-      true
-      (damp.ekeko/ekeko [?cu ?key ?child ?shouldbecu ?shouldbekey] 
-                        (reification/ast :CompilationUnit ?cu) 
-                        (reification/has ?key ?cu ?child) 
-                        (reification/has ?shouldbekey ?shouldbecu ?child)))))
+  ;TODO: might be nice to have it work for other values (and nil) as well,
+  ;possible if reification of primitive values (and nil) is changed ... but comes at a high performance cost
+  ;(especially when nil has to be wrapped)
+  reification-has-relational
+  (doseq [[node property child shouldbenode shouldbeproperty]  
+          (damp.ekeko/ekeko [?node ?property ?child ?shouldbenode ?shouldbeproperty] 
+                  (fresh [?kind]
+                         (reification/ast ?kind ?node) 
+                         (reification/has ?property ?node ?child) 
+                         (conde [(succeeds (instance? org.eclipse.jdt.core.dom.ASTNode ?child))]
+                                [(succeeds (instance? java.util.List ?child))]) 
+                         (reification/has ?shouldbeproperty ?shouldbenode ?child)))]
+    (is (identical? node shouldbenode)
+    (is (identical? property shouldbeproperty)))))
+
+
+(deftest
+  ^{:doc "Tests whether ast/2 quantifies over each node encountered during 
+          a recursive descent of all compilation units."}
+   reification-ast-traversal
+  (test/tuples-are 
+    (damp.ekeko/ekeko [?ast]
+                      (fresh [?kind]
+                             (reification/ast ?kind ?ast)))
+    (damp.ekeko/ekeko [?ast]
+                      (fresh [?cu]
+                             (reification/ast :CompilationUnit ?cu)
+                             (conde [(== ?cu ?ast)]
+                                    [(reification/child+ ?cu ?ast)])))))
 
 ;; Test suite
 
-(deftest
-   test-suite 
-   (test/against-project-named "TestCase-JDT-CompositeVisitor" false reification-test))
 
+(deftest
+  test-suite
+  (let [visitorproject "TestCase-JDT-CompositeVisitor"]
+    (test/against-project-named visitorproject false reification-has-relational)
+    (test/against-project-named visitorproject false reification-ast-traversal)))
+  
 (defn 
   test-ns-hook 
   [] 

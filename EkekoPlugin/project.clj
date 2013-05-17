a476bcf803d2808a3288cc67dff33a9b5312fac7
Coen De Roover <cderoove@vub.ac.be>
2013-05-17 12:57:33 +0200

    Included EPL v1.0

--- EkekoPlugin/project.clj
+++ EkekoPlugin/project.clj
@@ -1,9 +1,21 @@
-(defproject Ekeko "1.0.2"
+(defproject Ekeko "1.0.3"
   :description "Applicative logic meta-programming using core.logic against an Eclipse workspace."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   ;:dev-dependencies [[lein-autodoc "0.9.0"]]
   ;:dev-dependencies [[codox "0.6.1"]]
+  
+    :codox {
+            :name "Ekeko"
+            :version "1.0.3"
+            :description "Applicative logic meta-programming using core.logic against an Eclipse workspace."
+            :output-dir "/Users/cderoove/Desktop/docekeko"
+            :sources ["src"]
+            :src-dir-uri "https://github.com/cderoove/damp.ekeko/blob/master/EkekoPlugin"
+            :src-linenum-anchor-prefix "L"}
+
+  
+  
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.7.5"]
                  [qwal "1.0.0-SNAPSHOT"]])

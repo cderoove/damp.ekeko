801f100f282647ceabf878c44a89492c9b098319
Coen De Roover <cderoove@vub.ac.be>
2013-08-20 12:40:04 +0200

    Excluding clojure.core.class from namespace declaration.

--- EkekoDemo/src/damp/ekeko/demo/relations.clj
+++ EkekoDemo/src/damp/ekeko/demo/relations.clj
@@ -1,6 +1,6 @@
 (ns 
   damp.ekeko.demo.relations
-  (:refer-clojure :exclude [== type])
+  (:refer-clojure :exclude [== type class])
   (:use [clojure.core.logic])
   (:use [damp.ekeko.logic])
   (:use [damp.ekeko.jdt.reification])
@@ -24,12 +24,23 @@
          (ast-encompassing-method ?s ?m)
          (has :returnType2 ?m ?t)))
 
-(defn  method-returning-null-for-ctype  [?method ?ast-type]  (fresh [?ast-type-kind ?collection-type ?type]         (type-qualifiedname ?collection-type "java.util.Collection")         (method-returning-null-for-type ?method ?ast-type)         (ast-type-type ?ast-type-kind ?ast-type ?type)         (type-super-type ?type ?collection-type)))
+;(defn;  method-returning-null-for-ctype;  [?method ?ast-type];  (fresh [?ast-type-kind ?collection-type ?type]
+;         (type-qualifiedname ?collection-type "java.util.Collection");         (method-returning-null-for-type ?method ?ast-type);         (ast-type-type ?ast-type-kind ?ast-type ?type);         (type-super-type ?type ?collection-type)
+;         ))
+
+(defn
+  method-returning-null-for-ctype
+  [?method ?ast-type]
+  (fresh [?ast-type-kind ?collection-type ?type]
+         (method-returning-null-for-type ?method ?ast-type)
+         (ast-type-type ?ast-type-kind ?ast-type ?type)
+         (type-qualifiedname ?ast-type "java.util.List")))
 
 (defn
   method-always-returning-null-for-ctype
   [?m ?t]
-  (method-returning-null-for-ctype ?m ?t))
+  (method-returning-null-for-type ?m ?t)
+  )
     
 ;  (defn;  method-always-returning-null-for-ctype;  [?m ?t];  (fresh [?cfg];         (method-returning-null-for-ctype ?m ?t);         (method-cfg ?m ?cfg) ;         (method-cfg-entry ?m ?entry);         (qwal ?cfg ?entry ?end [];         (qall 
 ;            (qcurrent [?return]

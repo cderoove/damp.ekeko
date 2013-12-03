aa7cd3063481f92748ef78cc4ff52088f5cb45a6
Coen De Roover <cderoove@vub.ac.be>
2013-12-03 20:42:02 +0100

    Cleaned up code.

--- EkekoVisualizer/src/damp/ekeko/visualization/view.clj
+++ EkekoVisualizer/src/damp/ekeko/visualization/view.clj
@@ -14,11 +14,48 @@
            [org.eclipse.jface.resource JFaceResources LocalResourceManager]
            [org.eclipse.swt.graphics RGB]
            [org.eclipse.draw2d Figure Ellipse FlowLayout Label MarginBorder StackLayout]
-
            ))
 
-;; Opening a view
-;; --------------
+
+(defmacro ui [& exps] `(gui/eclipse-uithread-return (fn [] ~@exps)))
+
+(defmacro
+  defnui 
+  [name argvector & exps]  
+  `(defn ~name ~argvector(ui ~@exps)))
+  
+(defnui
+  graph-nodes
+  [graph]
+  (.getNodes graph))
+
+
+(def layout|tree (ui (new TreeLayoutAlgorithm  LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+(def layout|horizontaltree (ui (new HorizontalTreeLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+(def layout|radial (ui (new RadialLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+(def layout|grid (ui (new GridLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+(def layout|spring (ui (new SpringLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+(def layout|shift (ui (new HorizontalShift LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
+
+(defn-
+  shift-layout
+  [layout]
+  (ui 
+    (CompositeLayoutAlgorithm.  LayoutStyles/NO_LAYOUT_NODE_RESIZING
+                              (into-array LayoutAlgorithm [layout|shift layout]))))
+(defnui
+  graph-layout!
+  [graph]
+  (.applyLayout graph))
+
+(def edge|dash (ZestStyles/CONNECTIONS_DASH))
+(def edge|dashdot (ZestStyles/CONNECTIONS_DASH_DOT))
+(def edge|directed (ZestStyles/CONNECTIONS_DIRECTED))
+(def edge|dot (ZestStyles/CONNECTIONS_DOT))
+(def edge|solid (ZestStyles/CONNECTIONS_SOLID))
+
+;; Opening a model-based graph view
+;; --------------------------------
 
 (def graph-view-cnt (atom 0))
 
@@ -31,145 +68,12 @@
         viewpart (.showView page viewid uniqueid (org.eclipse.ui.IWorkbenchPage/VIEW_ACTIVATE))]
     (swap! graph-view-cnt inc)
     (.setViewID viewpart uniqueid)
-    (.getGraph viewpart)))
+    viewpart))
 
 (defn
   open-graph-view
   []
   (gui/eclipse-uithread-return (fn [] (show-graph-view))))
-
-
-;; Graph manipulation
-;; ------------------
-
-(defmacro ui [& exps] `(gui/eclipse-uithread-return (fn [] ~@exps)))
-
-(defmacro
-  defnui 
-  [name argvector & exps]  
-  `(defn ~name ~argvector(ui ~@exps)))
-
-  
-(defnui
-  graph-nodes
-  [graph]
-  (.getNodes graph))
-
-(defnui
-  graph-edges
-  [graph]
-  (.getConnections graph))
-
-(defnui 
-  graph-add-node!
-  [graph]
-  ;create anonymous subclass of Zest's GraphNode to prevent toString from being called outside of the UI thread by the REPL
-    (proxy
-      [GraphNode] 
-      [graph (.getNodeStyle graph)]
-      (toString []
-        ;needs to be called in UI thread
-        (ui (proxy-super toString)))))
-
-(defnui
-  node-set-text!
-  [node string]
-  (.setText node string))
-
-(defnui
-  graph-add-edge!
-  [graph from to]
-  ;same as above
-  (proxy 
-    [GraphConnection]
-    [graph (.getConnectionStyle graph) from to]
-    (toString []
-        ;needs to be called in UI thread
-        (ui (proxy-super toString)))))
-
-(defnui
-  edge-set-text!
-  [node string]
-  (.setText node string))
-
-(defnui
-  graph-set-layout!
-  [graph layout]
-  ;note that the layout algorithm has to be created in the ui thread!
-  (.setLayoutAlgorithm graph layout true))
-
-
-(def layout|tree (ui (new TreeLayoutAlgorithm  LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-(def layout|horizontaltree (ui (new HorizontalTreeLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-(def layout|radial (ui (new RadialLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-(def layout|grid (ui (new GridLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-(def layout|spring (ui (new SpringLayoutAlgorithm LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-(def layout|shift (ui (new HorizontalShift LayoutStyles/NO_LAYOUT_NODE_RESIZING)))
-;compisite layout ontbreekt, nodig om bv tree-layout te combineren met horizontal-shift-layout zodat de nodes niet overlappen
-
-(defn-
-  shift
-  [layout]
-  (ui 
-    (CompositeLayoutAlgorithm.  LayoutStyles/NO_LAYOUT_NODE_RESIZING
-                              (into-array LayoutAlgorithm [layout|shift layout]))))
-  
-
-       
-
-(defnui
-  graph-layout!
-  [graph]
-  (.applyLayout graph))
-
-
-
-
-(comment
-  
-  (def g (open-graph-view))
-  
-  (def root (graph-add-node! g))
-  
-  (dotimes [n 10] 
-    (let [child (graph-add-node! g)]
-      (node-set-text! child (str n))
-      (graph-add-edge! g child root)))
-  
-  (graph-set-layout! g layout|tree) 
-  (graph-set-layout! g layout|spring)                 
-  
-  )
-
-
-(def edge|dash (ZestStyles/CONNECTIONS_DASH))
-(def edge|dashdot (ZestStyles/CONNECTIONS_DASH_DOT))
-(def edge|directed (ZestStyles/CONNECTIONS_DIRECTED))
-(def edge|dot (ZestStyles/CONNECTIONS_DOT))
-(def edge|solid (ZestStyles/CONNECTIONS_SOLID))
-
-
-
-;; Opening a model-based view
-;; --------------------------
-
-(def mgraph-view-cnt (atom 200))
-
-(defn-
-  show-mgraph-view
-  []
-  (let [page (gui/workbench-activepage)
-        viewid (damp.ekeko.gui.views.EkekoVisualizationView/ID)
-        uniqueid (str @mgraph-view-cnt)
-        viewpart (.showView page viewid uniqueid (org.eclipse.ui.IWorkbenchPage/VIEW_ACTIVATE))]
-    (swap! mgraph-view-cnt inc)
-    (.setViewID viewpart uniqueid)
-    viewpart))
-
-(defn
-  open-mgraph-view
-  []
-  (gui/eclipse-uithread-return (fn [] (show-mgraph-view))))
 
 
 (defrecord ModelBasedView [content label layout listener])
@@ -278,7 +182,7 @@
         (getLineWidth [src dest]
           (edge|width src dest))
         )
-    (shift layout)
+    (shift-layout layout)
     (proxy 
       [ISelectionChangedListener]
       []
@@ -289,68 +193,6 @@
             (nil? selected) (void|selected)
             (instance? EntityConnectionData selected) (edge|selected selected)
             :else (node|selected selected)))))))
-
-;; Query-based views (probably broken)
-;; ===================================
-
-(declare open-view-with-context)
-
-;=> (group-by first [ [1 :a] [1 :b] [1 :c]  [2 :d] [2 :e]    ])
-;{1 [[1 :a] [1 :b] [1 :c]], 2 [[2 :d] [2 :e]]}
-
-(defn
-  qview
-  [node1tuples edge2tuples]
-  (let [
-        edgemap
-        (group-by first edge2tuples)
-        
-        context 
-        (ModelBasedView.     
-          (proxy 
-              [ArrayContentProvider IGraphEntityContentProvider]
-              []
-              (getConnectedTo [element] 
-                (into-array (map second (get edgemap element)))))
-          (proxy 
-            [LabelProvider] 
-            
-            ;implementatie best als een dictionary met symbol als key, function als value
-              ;de dict van de gebruiker mergen met een dict van defaults (waarvan de meeste functies null teruggeven)
-              
-              ;extra interfaces:  
-              ;IEntityStyleProvider getBackground, borderWidth, .... om fill color, border width van nodes te veranderen
-              ;IEntityConnectionStyleProvider connection style van edges 
-              
-              
-              ;of 1 alternatieve interface:
-              ;alle nodes en edges stylen ahv interface ISelfStyleProvider, waarin setBackGroundColor etc opgeroepen kan worden op elke edge/node
-              
-              
-              []
-              (getText [object]
-                (if
-                  (instance? EntityConnectionData object)
-                  ""
-                  ; (str (.source object) "->" (.dest object)) 
-                  (str (.getClass object))))
-              ;te overriden van LabelProvider: getImage om figuren in de nodes te tonen
-              ;daarna extra interface methods implementeren
-              )
-          spring-layout 
-          nil
-          
-          )
-        
-        nodes
-        (map first node1tuples)
-        ]
-    
-    (open-view-with-context nodes context)
-    
-    ))
-          
-
 
 (defn
   set-view-elements!
@@ -375,7 +217,7 @@
 (defn
   open-view-on-elements-with-context
   [elements context]
-  (let [view (open-mgraph-view)]
+  (let [view (open-graph-view)]
     (ui 
       (set-view-context! view context)
       (set-view-elements! view elements))))
@@ -402,7 +244,7 @@
      elements
      (range 10)
      view
-     (open-mgraph-view)
+     (open-graph-view)
      context 
      (make-graphicalcontext 
        :connections
@@ -463,10 +305,6 @@
        (fn [edge] (println "Selected edge: " edge))
        :void|selected
        (fn [] (println "Nothing selected anymore"))
-       
-       
-       
-       
        )
      ]
     

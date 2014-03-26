e4118a20fc15cf4e0e0a716f892d12dd5f0a1502
Tim Molderez <id@timmolderez.be>
2014-03-26 03:34:05 +0100

    Added support for Inspector Jay
    The results of Ekeko queries can now be inspected in Inspector Jay (a
    Java/Clojure object inspector).
    
    To give it a whirl: (use 'damp.ekeko.inspector.inspector)
    You can then write Ekeko queries as usual, but using the ekeko+ function
    (instead of ekeko / ekeko*).

---
+++ EkekoPlugin/src/damp/ekeko/inspector/inspector.clj
@@ -0,0 +1,39 @@
+(ns 
+  ^{:doc "Inspect the results of Ekeko queries with an Inspector Jay view.
+          https://github.com/timmolderez/inspector-jay"
+    :author "Tim Molderez"}
+  damp.ekeko.inspector.inspector
+  (:refer-clojure :exclude [== type])
+  (:use clojure.core.logic)
+  (:use [damp.ekeko logic])
+  (:use [damp.ekeko.soot soot])
+  (:use [damp.ekeko.jdt ast structure aststructure soot convenience])
+  (:use [damp.qwal])
+  (:use [inspector-jay.core])
+  (:use [inspector-jay.gui.gui])
+  (:require [damp.ekeko.util [text :as text]])
+  (:require [damp.ekeko [gui :as gui]]))
+
+; Give each view instance a unique id
+(def inspector-view-cnt (atom 0))
+
+(defn get-inspector-view [results]
+  "Build a ViewPart with an Inspector Jay panel that inspects 'results'"
+  (let [page (gui/workbench-activepage)
+        viewid (damp.ekeko.inspector.JPanelView/ID)
+        uniqueid (str @inspector-view-cnt)
+        viewpart (.showView page viewid uniqueid (org.eclipse.ui.IWorkbenchPage/VIEW_ACTIVATE))]
+    (swap! inspector-view-cnt inc)
+    (-> viewpart (.setJPanel (inspector-panel results)))
+    viewpart))
+
+(defmacro 
+  ekeko+
+  "Runs an Ekeko query and opens an Inspector Jay view to inspect the results
+   See also: damp.ekeko/ekeko"
+  [vars & goals]
+  `(let [resultsqc# (doall (run-nc* [resultvar#] 
+                             (fresh [~@vars]
+                               (equals resultvar# [~@vars])
+                               ~@goals)))]
+     (gui/eclipse-uithread-return (fn [] (get-inspector-view resultsqc#)))))

7481720096fffb616e3424816b509909268c419f
Coen De Roover <cderoove@vub.ac.be>
2014-10-23 06:32:28 -0700

    Added the workspace listeners and markers from the demo.

--- damp.ekeko.plugin/src/damp/ekeko/ekekomodel.clj
+++ damp.ekeko.plugin/src/damp/ekeko/ekekomodel.clj
@@ -5,7 +5,11 @@
   (:import 
     [org.eclipse.jdt.core  IMember IJavaElement ITypeHierarchy JavaCore IType IJavaModel IJavaProject IPackageFragment ICompilationUnit]
     [org.eclipse.jdt.core.dom ConstructorInvocation SuperConstructorInvocation ClassInstanceCreation SuperMethodInvocation IMethodBinding AnonymousClassDeclaration IBinding MethodDeclaration MethodInvocation ASTParser AST ASTRequestor CompilationUnit ASTNode TypeDeclaration]
-    [damp.ekeko EkekoModel JavaProjectModel ProjectModel]))
+    [damp.ekeko EkekoModel JavaProjectModel ProjectModel])
+  (:import 
+    [org.eclipse.jdt.core.dom ASTNode]
+    [baristaui.util MarkerUtility]
+    [damp.ekeko IEkekoModelUpdateListener EkekoModelRemovedEvent]))
 
 
 (def #^{:dynamic true
@@ -75,6 +79,96 @@
 (defn remove-all-queried-models []
   (.clear (all-project-models)))
 
+
+
+(defn
+  model-update-listener
+  [execute-upon-change]
+  (reify 
+    IEkekoModelUpdateListener
+    (projectModelUpdated [this model]
+                         (when
+                           (instance? EkekoModelRemovedEvent model)
+                           (do
+                             (println "Project model no longer being queried:" model)
+                             (.removeListener (ekeko-model) this)))
+                         (execute-upon-change model))))
+
+(defn-
+  incremental-node-manager
+  [node-retrieval-function
+   nodes-added-function 
+   nodes-removed-function]
+  (let [previous-nodes (atom #{})]
+    (fn [event]
+      (let [nodes (into #{} (node-retrieval-function))]
+        (nodes-removed-function (clojure.set/difference @previous-nodes nodes))
+        (nodes-added-function (clojure.set/difference nodes @previous-nodes))
+        (reset! previous-nodes nodes)))))
+
+
+(defn
+  incremental-node-printer
+  [node-retrieval-function]
+  (incremental-node-manager 
+    node-retrieval-function
+    (fn [added] (println "Found "  (count added) " new nodes."))
+    (fn [removed] (println "Found "  (count removed) " removed nodes."))))
+
+
+(defn
+  marker-utility
+  []
+  (MarkerUtility/getInstance))
+
+
+(defn
+  incremental-marker-updater
+  [node-retrieval-function]
+  (let [node-to-marker (atom {})]
+    (incremental-node-manager 
+      node-retrieval-function
+      (fn [added]
+        (swap!
+         node-to-marker
+         merge
+         (zipmap
+           added
+           (map (fn [n]
+                  (.tempNewMarker ^MarkerUtility (marker-utility) n))
+                added))))
+      (fn [removed]
+        (doseq [n removed]
+          (let [marker (get @node-to-marker n)]
+            (when
+              (.exists marker)
+              (.delete marker))))))))
+
+(defn
+  incremental-node-marker
+  [node-retrieval-function]
+  (let [update-markers
+        (incremental-marker-updater node-retrieval-function)]
+    (fn [model]
+      (update-markers model))))
+
+(comment
+  
+  (def 
+    listener
+    (model-update-listener 
+      (incremental-node-marker
+        (fn []
+          (map first 
+            (damp.ekeko/ekeko [?m] 
+             (damp.ekeko.jdt.ast/ast :MethodDeclaration ?m)))))))
+  
+  (register-listener listener)
+  ;change file
+  (unregister-listener listener)
+   
+  
+  )
 
 
 
